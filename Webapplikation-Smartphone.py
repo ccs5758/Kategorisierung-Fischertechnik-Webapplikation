dec333e80b2915eccbc1bc43cb69f938b32b2370
ccs5758 <constantin.steffens@tuhh.de>
2024-08-21 16:53:57 +0200

    Add files via upload

--- Webapplikation-Smartphone.py
+++ Webapplikation-Smartphone.py
@@ -1,17 +1,17 @@
 import streamlit as st 
-#import keras 
-#import tensorflow
+import keras 
+import tensorflow
 import numpy as np
 from PIL import Image
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-#st.set_option('deprecation.showfileUploaderEncoding', False)
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
 CATEGORIES = ["semi-finished product", "finished product"]
 
 
-#model = keras.saving.load_model("image_classifier_custom_binary_3.keras")
+model = keras.saving.load_model("image_classifier_custom_binary_3.keras")
 img_data = st.camera_input(label='load image')
 
 if img_data: 
@@ -24,6 +24,6 @@ if img_data:
     img_data = img_data/255.0
     new_image=np.array(img_data).reshape(-1,IMG_SIZE,IMG_SIZE,1)
     st.image(img_data)
-    #prediction = model.predict([new_image])
-    #st.title(int(prediction[0][0]))
-    #st.title("prediction is " & CATEGORIES[int(prediction[0][0])])
+    prediction = model.predict([new_image])
+    st.title(int(prediction[0][0]))
+    st.title("prediction is " & CATEGORIES[int(prediction[0][0])])
